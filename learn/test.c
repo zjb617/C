6978a7e3c460a0af8c5e8a969393fb6e32f4a925
周金波 <2575442820@qq.com>
2021-04-04 00:37:12 +0800

    changes

--- learn/test.c
+++ learn/test.c
@@ -1,9 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 int main(void)
 {
-   int i, k;
-   for (i = 3; i <= 4; i++)
-      for (k = 1; k <= 2; k++)
-         printf("*");
-   printf("#");
+   int age;
+   printf("请输入你的年龄\n");
+   scanf("%d", &age);
+   if (age >= 18)
+   {
+      printf("你可以使用此软件");
+   }
+   else
+   {
+      printf("你年龄不够，不可使用此软件");
+   }
+   return 0;
 }
---
+++ learn/test2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+	int age;
+	printf("请输入你的年龄\n");
+	scanf("%d", &age);
+	if (age >= 18) 
+    {
+		printf("你可以使用此软件");
+	}
+	else 
+    {
+		printf("你年龄不够，不可使用此软件");
+	}
+	return 0;
+}
